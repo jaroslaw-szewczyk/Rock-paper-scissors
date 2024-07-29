4f34d1700fa71d350f0097e03dbdb071babe714b
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-07-29 14:20:00 +0100

    Add computer move

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let computerMove = Math.floor(Math.random() * (19 - 11 + 1) + 11);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
+console.log("the computer has drawn a number " + computerMove);
+
+if(computerMove == 1) {
+    computerMove = "Rockk";
+} else if(computerMove == 2) {
+    computerMove == "Paper";
+} else {
+    computerMove == "scissors";
+}
