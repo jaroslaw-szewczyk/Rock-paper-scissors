f97d3f27e77a060a1ed1e3e59acd652273490626
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-07-29 17:13:58 +0100

    add functions in js code

--- js/script.js
+++ js/script.js
@@ -1,44 +1,35 @@
-let computerMove = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = prompt("what is your ryuch? (1 - stone, 2 - paper, 3 - scissors)");
-
-console.log("the computer has drawn a number " + computerMove);
-console.log("player selected  " + playerMove);
-
-if(computerMove == 1) {
-    computerMove = "rock";
-} else if(computerMove == 2) {
-    computerMove ="paper";
-} else if(computerMove == 3){
-    computerMove = "scissors";
-} else {
-    computerMove = "incorrect choice";
-}
-
-if(playerMove == 1) {
-    playerMove = "rock";
-} else if(playerMove == 2) {
-    playerMove = "paper";
-} else if(playerMove == 3) {
-    playerMove = "scissors";
-} else {
-    playerMove = "incorrect choice";
-}
-
-console.log("the computer has drawn a number " + computerMove);
-console.log("player selected  " + playerMove);
-
-if(computerMove == "rock" && playerMove == "paper"){
-    printMessage("You win");
-  } else if( computerMove == "scissors" && playerMove == "rock"){
-    printMessage("You win");
-  } else if( computerMove == "paper" && playerMove == "scissors"){
-    printMessage("You win");
-  } else if (computerMove == playerMove){
-    printMessage("Draw");
-  } else if (computerMove == "incorrect choice" || playerMove == "incorrect choice") {
-    printMessage("Incorrect choice. Computer: " + computerMove + ", player: " + playerMove);
-  } else {
-    printMessage("You lose");
-  }
+function getMoveName(argMoveId){
+    if(argMoveId == 1) {
+        return "rock";
+    } else if(argMoveId == 2) {
+        return "paper";
+    } else if(argMoveId == 3){
+        return "scissors";
+    } 
   
+    return 'incorrect choice';
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+    if(argComputerMove == "rock" && argPlayerMove == "paper"){
+        printMessage("You win");
+      } else if( argComputerMove == "scissors" && argPlayerMove == "rock"){
+        printMessage("You win");
+      } else if( argComputerMove == "paper" && argPlayerMove == "scissors"){
+        printMessage("You win");
+      } else if (argComputerMove == argPlayerMove){
+        printMessage("Draw");
+      } else if(argComputerMove == "incorrect choice" || argPlayerMove == "incorrect choice"){
+        printMessage("Incorrect choice. <br>Computer: " + argComputerMove + " <br>Player: " + argPlayerMove);
+      } else {
+        printMessage("You lose");
+      }
+}
+
+let computerMove = getMoveName(Math.floor(Math.random() * 3 + 1));
+
+let playerChoice = prompt("what is your ryuch? (1 - stone, 2 - paper, 3 - scissors)");
+let playerMove = getMoveName(playerChoice);
+
+displayResult(computerMove, playerMove);
