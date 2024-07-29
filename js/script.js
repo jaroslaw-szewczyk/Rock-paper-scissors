f15986790641219918ad4f79193dea80b4e9e7e8
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-07-29 14:27:32 +0100

    Add player move

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-let computerMove = Math.floor(Math.random() * (19 - 11 + 1) + 11);
+let computerMove = Math.floor(Math.random() * 3 + 1);
+
+let playerMove = prompt("what is your ryuch? (1 - stone, 2 - paper, 3 - scissors)");
 
 console.log("the computer has drawn a number " + computerMove);
+console.log("player selected  " + playerMove);
 
 if(computerMove == 1) {
     computerMove = "Rockk";
@@ -8,4 +11,12 @@ if(computerMove == 1) {
     computerMove == "Paper";
 } else {
     computerMove == "scissors";
+}
+
+if(playerMove == 1) {
+    playerMove = "Rockk";
+} else if(playerMove == 2) {
+    playerMove == "Paper";
+} else {
+    playerMove == "scissors";
 }
