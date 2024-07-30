48660fa450b919b80230c9cfe7b2e543c622fce9
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-07-30 14:09:19 +0100

    Add btn and event listener

--- js/script.js
+++ js/script.js
@@ -1,35 +1,59 @@
+let rockBtn = document.getElementById("rock");
+let paperBtn = document.getElementById("paper");
+let scissorsBtn = document.getElementById("scissors");
 
-function getMoveName(argMoveId){
-    if(argMoveId == 1) {
-        return "rock";
-    } else if(argMoveId == 2) {
-        return "paper";
-    } else if(argMoveId == 3){
-        return "scissors";
-    } 
-  
-    return 'incorrect choice';
+
+
+
+function playGame(playerChoice) {
+   
+  clearMessages();
+
+  function getMoveName(argMoveId){
+      if(argMoveId == 1) {
+          return "rock";
+      } else if(argMoveId == 2) {
+          return "paper";
+      } else if(argMoveId == 3){
+          return "scissors";
+      } 
+    
+      return 'incorrect choice';
+  }
+
+  function displayResult(argComputerMove, argPlayerMove){
+      if(argComputerMove == "rock" && argPlayerMove == "paper"){
+          printMessage("Computer: " + argComputerMove + "<br>Player: " + argPlayerMove +"<br>You win");
+        } else if( argComputerMove == "scissors" && argPlayerMove == "rock"){
+          printMessage("Computer: " + argComputerMove + "<br>Player: " + argPlayerMove +"<br>You win");
+        } else if( argComputerMove == "paper" && argPlayerMove == "scissors"){
+          printMessage("Computer: " + argComputerMove + "<br>Player: " + argPlayerMove +"<br>You win");
+        } else if (argComputerMove == argPlayerMove){
+          printMessage("Computer: " + argComputerMove + "<br>Player: " + argPlayerMove +"<br>Draw");
+        } else if(argComputerMove == "incorrect choice" || argPlayerMove == "incorrect choice"){
+          printMessage("Incorrect choice. <br>Computer: " + argComputerMove + " <br>Player: " + argPlayerMove);
+        } else {
+          printMessage("Computer: " + argComputerMove + "<br>Player: " + argPlayerMove +"<br>You lose");
+        }
+  }
+
+  let computerMove = getMoveName(Math.floor(Math.random() * 3 + 1));
+
+  // let playerChoice = prompt("what is your ryuch? (1 - stone, 2 - paper, 3 - scissors)");
+  playerMove = getMoveName(playerChoice);
+
+  displayResult(computerMove, playerMove);
 }
 
-function displayResult(argComputerMove, argPlayerMove){
-    if(argComputerMove == "rock" && argPlayerMove == "paper"){
-        printMessage("You win");
-      } else if( argComputerMove == "scissors" && argPlayerMove == "rock"){
-        printMessage("You win");
-      } else if( argComputerMove == "paper" && argPlayerMove == "scissors"){
-        printMessage("You win");
-      } else if (argComputerMove == argPlayerMove){
-        printMessage("Draw");
-      } else if(argComputerMove == "incorrect choice" || argPlayerMove == "incorrect choice"){
-        printMessage("Incorrect choice. <br>Computer: " + argComputerMove + " <br>Player: " + argPlayerMove);
-      } else {
-        printMessage("You lose");
-      }
-}
 
-let computerMove = getMoveName(Math.floor(Math.random() * 3 + 1));
+rockBtn.addEventListener('click', ()=> {
+  playGame(1);
+});
 
-let playerChoice = prompt("what is your ryuch? (1 - stone, 2 - paper, 3 - scissors)");
-let playerMove = getMoveName(playerChoice);
+paperBtn.addEventListener('click', ()=> {
+  playGame(2);
+});
 
-displayResult(computerMove, playerMove);
+scissorsBtn.addEventListener('click', ()=> {
+  playGame(3);
+});
